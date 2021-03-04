1b85ecc555793c30e478642c2379d6b42f612397
= <=>
2021-03-04 13:56:55 +0100

    feat: add join group form

---
+++ app/src/components/forms/index.js
@@ -0,0 +1,1 @@
+export {default as JoinGroupForm} from './join-group-form/JoinGroupForm';
---
+++ app/src/components/popups/popup/Popup.vue
@@ -0,0 +1,19 @@
+<template>
+  <Overlay @onLeave="$emit('onLeave')">
+    <div class="bg-white rounded-lg w-4/5 lg:w-2/3 mx-auto">
+      <slot/>
+    </div>
+  </Overlay>
+</template>
+
+<script>
+import Overlay from "./local-components/Overlay";
+export default {
+name: "Popup",
+  components: {Overlay}
+}
+</script>
+
+<style scoped>
+
+</style>
